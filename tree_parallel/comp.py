02f4aa3bec74320ff0a64b0ccfcca17f473da0bd
starrysky <chiangyiting526@gmail.com>
2025-06-08 18:21:04 +0800

    parallelize the for-loop of evaluation in tree code

---
+++ tree_parallel/comp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv("force_tree_parallel.csv", sep=",", header=None)
+df2 = pd.read_csv("force_tree.csv", sep=",", header=None)
+
+error = ((df2 - df1) / df1)**2
+error = np.sqrt(error.sum(axis=0)) / (len(df1[0]))
+for i in range(3):
+    print("{:.10e}".format(error[i]))
