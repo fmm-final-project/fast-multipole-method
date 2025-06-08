06f820ca7ba0501e8a0f9debe71f9511731ab034
starrysky <chiangyiting526@gmail.com>
2025-06-09 02:45:51 +0800

    fmm openmp parallelization

---
+++ fmm_parallel/comp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv("force_tree2fmm_parallel.csv", sep=",", header=None)
+df2 = pd.read_csv("force_tree2fmm.csv", sep=",", header=None)
+
+error = ((df2 - df1) / df1)**2
+error = np.sqrt(error.sum()) / (len(df1[0]))
+for i in range(3):
+    print("{:.10e}".format(error[i]))
