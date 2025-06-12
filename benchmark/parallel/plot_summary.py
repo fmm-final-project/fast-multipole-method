73775ec648b2ebfab8684fda9690c6187f3c9b1a
LIN,CHIH-YUN <b10202037@ntu.edu.tw>
2025-06-12 08:16:22 +0800

    rerun parallel

--- benchmark/parallel/plot_summary.py
+++ benchmark/parallel/plot_summary.py
@@ -2,20 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-outfolder = 'fmm_output/'
-df = pd.read_csv(outfolder + "summary_results_fmm.csv")
-name = "FMM"
+outfolder = 'tree_output/'
+df = pd.read_csv(outfolder + "summary_results.csv")
+name = "Tree"
 
 time_unit = 1000
 
 # ----------- Plot 1: Time vs nThreads -----------
 plt.figure(figsize=(8, 6))
-plt.plot(df["nThreads"], df["Building time"]/time_unit, marker='o', label="Building time")
-plt.plot(df["nThreads"], df["Expansion time"]/time_unit, marker='s', label="Expansion time")
-plt.plot(df["nThreads"], df["Dual Tree Walk time"]/time_unit, marker='^', label="Dual Tree Walk time")
-plt.plot(df["nThreads"], df["Local Passing Down time"]/time_unit, marker='d', label="Local Passing Down time")
-plt.plot(df["nThreads"], df["Total execution time"]/time_unit, marker='x', label="Total execution time")
-plt.plot(df["nThreads"], df["Total execution time"][0]/time_unit/df["nThreads"], marker='x', label="Ideal execution Time")
+plt.plot(df["nThreads"][1:], df["Building time"][1:]/time_unit, marker='o', label="Building time")
+plt.plot(df["nThreads"][1:], df["Expansion time"][1:]/time_unit, marker='s', label="Expansion time")
+# plt.plot(df["nThreads"][1:], df["Dual Tree Walk time"][1:]/time_unit, marker='^', label="Dual Tree Walk time")
+# plt.plot(df["nThreads"][1:], df["Local Passing Down time"][1:]/time_unit, marker='d', label="Local Passing Down time")
+plt.plot(df["nThreads"][1:], df["Total execution time"][1:]/time_unit, marker='x', label="Total execution time")
+plt.plot(df["nThreads"][1:], df["Total execution time"][0]/time_unit/df["nThreads"][1:], '--', label="Ideal execution Time")
 
 
 plt.xlabel("nThreads")
@@ -30,8 +30,8 @@ plt.show()
 # ----------- Plot 2: SpeedUp vs nThreads -----------
 plt.figure(figsize=(8, 6))
 one_core_time = df["Total execution time"][0]
-plt.plot(df["nThreads"], one_core_time/df["Total execution time"], marker='o', label="SpeedUp")
-plt.plot(df["nThreads"], df["nThreads"], '--', label='Ideal (Linear SpeedUp)')
+plt.plot(df["nThreads"][1:], one_core_time/df["Total execution time"][1:], marker='o', label="SpeedUp")
+plt.plot(df["nThreads"][1:], df["nThreads"][1:], '--', label='Ideal (Linear SpeedUp)')
 plt.xlabel("nThreads")
 plt.ylabel("SpeedUp")
 plt.title(name + " SpeedUp vs nThreads")
